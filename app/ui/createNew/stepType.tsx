9c0f5284f0b1898bc49c851270cbda38d2964018
Dan Bock <danbock@gmail.com>
2026-01-31 12:17:48 -0500

    Fix spacing

--- app/ui/createNew/stepType.tsx
+++ app/ui/createNew/stepType.tsx
@@ -16,7 +16,7 @@ const StepType: FC<Props> = (props) => (
         props.setStep('name')
       }}
       className="w-full flex items-center justify-center h-14 bg-base-200 hover:bg-base-300 text-base-content p-4
-        rounded-lg border-0 cursor-pointer transition-colors"
+        rounded-lg border-0 cursor-pointer transition-colors mb-0"
     >
       <span className="flex-grow">Manual Collection</span>
     </button>
@@ -26,7 +26,7 @@ const StepType: FC<Props> = (props) => (
         props.setType('chess.com')
         props.setStep('username')
       }}
-      className="w-full flex items-center justify-center bg-chesscom p-2 rounded"
+      className="w-full flex items-center justify-center bg-chesscom p-2 mt-4 mb-0 rounded"
     >
       <img
         src="https://images.chesscomfiles.com/uploads/v1/images_users/tiny_mce/PedroPinhata/phpNgJfyb.png"
@@ -40,7 +40,7 @@ const StepType: FC<Props> = (props) => (
         props.setType('lichess')
         props.setStep('username')
       }}
-      className="w-full flex items-center justify-center gap-2 bg-lichess p-2 rounded"
+      className="w-full flex items-center justify-center gap-2 bg-lichess p-2 mt-4 rounded"
     >
       <Image src={lichessLogo} width={24} height={24} alt="Lichess" className="h-10 w-10 mr-2" />
       <span className="text-white">Lichess</span>
